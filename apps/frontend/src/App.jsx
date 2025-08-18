7e2f3b0e47fc792a67e2d8d8c48b32ab0f5ac88a
Aman Ahamed <amanahamed04gmail.com>
2025-08-18 14:04:38 +0530

    fe && be code add

---
+++ apps/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen grid place-items-center bg-gray-50">
+      <h1 className="text-3xl font-bold underline">Hello Tailwind + React</h1>
+    </div>
+  );
+}
+export default App;
